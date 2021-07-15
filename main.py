af14d784f901ac622de54847be0e3f2f99448f87
Areesha-Tahir <82057205+Areesha-Tahir@users.noreply.github.com>
2021-07-15 20:01:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import queue
 # to keep track of the blocks of maze
 class Grid_Position:
@@ -194,4 +191,3 @@ def main():
 if __name__ == '__main__':
     main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
